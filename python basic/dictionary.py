ec15a5227fc5c6e11f71d92954b568c92ab960c5
Shreevatsatg <tgshreevatsa@gmail.com>
2024-10-30 12:55:10 +0530

    added dictionary and sets practice files

---
+++ python basic/dictionary.py
@@ -0,0 +1,21 @@
+dict={
+    "name":"shreevatsa",
+    "cgpa":8.5,
+    "marks":[98,97,95],
+}
+print(dict)
+print(dict["name"]) #to access individual values
+dict["name"] ="shreevatsa tg"
+print(dict["name"])
+emptydict={}
+print(dict.keys())
+print(len(dict))
+print(dict.values())
+print(dict.items())
+
+
+
+
+
+
+
---
+++ python basic/sets.py
@@ -0,0 +1,22 @@
+
+#set do not  have dublicate items
+set={1,2,3,4}
+set2={2,3,4,5,6}
+print(set)
+print(type (set))
+# set() #empty set
+set.add(5)
+print(set)
+print(set.union(set2)) #union operation
+print(set.intersection(set2))#intersection operation
+
+#practice question
+
+dicti={
+    "table":["a piece of furniture","list of facts and figures"],
+    "cat":"a small animal"
+}
+
+set={"python","java","c++","python","javascript","java","python","java","c++","c"}
+print(len(set))
+#chapter 5 at 5:1 hour
