fefaa77452f0b8a60f103b8958ec1d05f2a83996
Shreevatsatg <tgshreevatsa@gmail.com>
2024-11-06 10:58:30 +0530

    added loops ractice file

--- python basic/loops.py
+++ python basic/loops.py
@@ -35,7 +35,83 @@
 #         print("number found at",idx+1)
 #     idx+=1
 
-# 5:31 hours done
+#break statent
+
+# i=0
+# while i<=5:
+#     print(i)
+#     if(i==3):
+#         break
+#     i+=1
+
+#continue statement
+
+# i=0
+# while i<=5:
+#     if(i==3):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+
+    
+# list =[1,2,3]
+# for a in list:
+#     print(a)
+
+# num=[1,2,3,4,5,6,7,8,9]
+# for val in num:
+#     print(val)
+
+# str ="SHREEVATSA"
+# for char in str :
+#     print(char)
+# else:
+#     print("the end")
+
+#practice questions
+
+# list=[1,4,9,16,25,36,49,64,81,100]
+# for el in list:
+#     print(el)
+
+# tup=(1,4,9,16,25,36,49,64,81,100)
+# x=int(input("enter the number "))
+# idx=0
+# for el in tup:
+#     if (el ==x):
+#         print("element found at index",idx)
+#         break
+#     idx+=1
+
+# for i in range(10): (stop)
+#     print(i)
+
+# for i in range(2,10): (start ,stop)
+#     print(i)
+
+#for i in range(2,10,2): (start,stop,step size)
+#print(i)
+
+# pass is used to null statent that does noting.it id used to place hold for a future code
+
+# for el in range(10):
+#     pass
+
+#practice questions
+
+# n=int(input("enter the number "))
+# sum =0
+# for i in range(1,n+1):
+#     sum +=i
+# print("total sum =",sum)
+
+# n=int(input("enter the number "))
+# sum =1
+# for i in range(1,n+1):
+#     sum *=i
+# print("total sum =",sum)
 
 
 
