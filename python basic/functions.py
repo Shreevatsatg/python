25d95d74a369fd7773cf2fa0a053f2a77ea053a9
Shreevatsatg <tgshreevatsa@gmail.com>
2024-11-06 10:58:45 +0530

    added functions practice file

---
+++ python basic/functions.py
@@ -0,0 +1,35 @@
+# def calcsum(a,b):               #function definition
+#     sum=a+b
+#     return sum
+
+# d=int(input("enter the first number"))
+# c=int(input("enter the second number"))
+# print(calcsum(d,c))                 #function call arguments
+
+#practice questions
+
+# cities =["dehi","guurgau","noida","pune","mumbai","chenai"]
+# heros=["thor","ironman","captain america","shakthiman"]
+
+# def printlength(list):
+#     print(len(list))
+
+# printlength(cities)
+# printlength(heros)
+
+# def printlist(list):
+#     for item in list:
+#         print(item,end=" ")
+
+# printlist(heros)
+# printlist(cities)
+
+
+# def calcfact(n):
+#     fact =1
+#     for i in range(1,n+1):
+#         fact *=i
+#     print(fact)
+
+# calcfact(6)
+
