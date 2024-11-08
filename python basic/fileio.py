8d0673c87a083956d627e10fdaef1baddfc3e86c
Shreevatsatg <tgshreevatsa@gmail.com>
2024-11-08 11:30:29 +0530

    added fileio practice questions

--- python basic/fileio.py
+++ python basic/fileio.py
@@ -1,1 +1,30 @@
- #chapter 7 7:5 hours
+# f=open("python basic/loops.py","r") #("file location","opening type r:read or w:write")
+# data =f.read()
+# print(data)
+# f.close()
+
+# f=open("python basic/fileioex.txt")
+# data =f.read(10)       #number of charecter to read and readline is used to read a single line
+# print(data)
+
+
+# f = open("python basic/fileioex.txt","w")
+# data = f.write("this is after useing write command to override the present text in the file")   
+# print(data)
+# f.close()
+
+# #syntax with
+# with open("python basic/fileioex.txt","r") as f:
+#     data = f.read()
+#     print(data)
+
+# import os #provides remove file option
+# os.remove("python basic/fileioex.txt")      #deletes the file
+
+# with open("practice.txt","r") as f: #if no file exists it will creates a new one by its own
+#     data= f.read()
+# newdata=data.replace("java","pyhton")
+# print(newdata)
+
+
+
